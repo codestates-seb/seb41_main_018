4b9129bc2d808eac3572d781b14102512460c4eb
Choiyu330 <jenny4063@naver.com>
2023-01-05 11:19:16 +0900

    first commit

---
+++ server/src/main/java/com/seb41_main_018/mainproject/MainprojectApplication.java
@@ -0,0 +1,13 @@
+package com.seb41_main_018.mainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainprojectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seb41_main_018/mainproject/MainprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb41_main_018.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
