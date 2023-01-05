f16e5795f645a17a8b10fb4eca9a4c15d53f85b1
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-01-05 17:56:51 +0900

    Feat: Validator#2
    Feat: Validator#2

---
+++ server/src/main/java/com/seb41_main_018/mainproject/response/SingleResponseDto.java
@@ -0,0 +1,10 @@
+package com.seb41_main_018.mainproject.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+}
