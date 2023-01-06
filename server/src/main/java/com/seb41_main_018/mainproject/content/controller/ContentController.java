507b8edc664c94b20a781f6559b7fbdd99605f15
Choiyu330 <jenny4063@naver.com>
2023-01-06 16:12:19 +0900

    refactor: ContentController / TagController 수정

--- server/src/main/java/com/seb41_main_018/mainproject/content/controller/ContentController.java
+++ server/src/main/java/com/seb41_main_018/mainproject/content/controller/ContentController.java
@@ -1,4 +1,4 @@
-package com.seb41_main_018.mainproject.post.controller;
+package com.seb41_main_018.mainproject.content.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -6,20 +6,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
-@RequestMapping("/post")
-public class PostController {
+@RequestMapping("/content")
+public class ContentController {
 
     // 게시글 생성
     @PostMapping
-    public ResponseEntity postPost(
+    public ResponseEntity postContent(
             // TODO: DTO
     ) {
         return null;
     }
 
     // 게시글 단건 조회
-    @GetMapping("/{postId}")
-    public ResponseEntity getPost(@PathVariable("postId") Long postId
+    @GetMapping("/{contentId}")
+    public ResponseEntity getContent(@PathVariable("contentId") Long contentId
                                   // TODO: DTO
     ) {
         return null;
@@ -27,22 +27,22 @@ public class PostController {
 
     // 게시글 전체 조회
     @GetMapping
-    public ResponseEntity getPosts(
+    public ResponseEntity getContents(
     ) {
         return null;
     }
 
     // 게시글 수정
-    @PatchMapping("/{postId}")
-    public ResponseEntity patchPost(@PathVariable("postId") Long postId
+    @PatchMapping("/{contentId}")
+    public ResponseEntity patchContent(@PathVariable("contentId") Long contentId
                                     // TODO: DTO
     ) {
         return null;
     }
 
     // 게시글 삭제
-    @DeleteMapping("/{postId}")
-    public ResponseEntity deletePost(@PathVariable("postId") Long postId
+    @DeleteMapping("/{contentId}")
+    public ResponseEntity deleteContent(@PathVariable("contentId") Long contentId
                                      // TODO: DTO
     ) {
         return null;
--- server/src/main/java/com/seb41_main_018/mainproject/tag/controller/TagController.java
+++ server/src/main/java/com/seb41_main_018/mainproject/tag/controller/TagController.java
@@ -1,4 +1,4 @@
-package com.seb41_main_018.mainproject.tag.entity;
+package com.seb41_main_018.mainproject.tag.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
