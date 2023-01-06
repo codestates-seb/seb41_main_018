fb8ba91f4931f470d80c24c4bf72471469da3cf5
kangharyeom <kangharyeom@gmail.com>
2023-01-06 14:24:56 +0900

    BE: Feat CategoryController 추가

---
+++ server/src/main/java/com/seb41_main_018/mainproject/category/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package com.seb41_main_018.mainproject.category.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+@Validated
+public class CategoryController {
+    @PostMapping("/{categoryId}")
+    public ResponseEntity postCategory(@PathVariable("categoryId") Long categoryId
+    ){
+        return null;
+    }
+
+    @PatchMapping("/{categoryId}")
+    public ResponseEntity patchCategory(@PathVariable("categoryId") Long categoryId){
+        return null;
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity getCategory(@PathVariable("categoryId") Long categoryId) {
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity getCategories() {
+        return null;
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity deleteCategory(@PathVariable("categoryId") Long categoryId) {
+        return null;
+    }
+}
