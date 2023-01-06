4e17dbb6cc39c8bd2711dbee6b1862b84e17f465
ahn <201717504@kyonggi.ac.kr>
2023-01-06 11:47:29 +0900

    feat: User entity 구현

---
+++ server/src/main/java/com/seb41_main_018/mainproject/user/entity/User.java
@@ -0,0 +1,47 @@
+package com.seb41_main_018.mainproject.user.entity;
+
+import lombok.Getter;
+import org.springframework.data.domain.Auditable;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String ninkname;
+    @Column(columnDefinition = "TEXT")
+    private Boolean email_subscribe;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private loginStatus loginstatus = loginStatus.USER_ACTIVE;
+
+    public enum loginStatus {
+
+        USER_ACTIVE("Active account"),
+        USER_INACTIVE("Inactive account"),
+        USER_QUIT("Deleted account");
+
+        @Getter
+        private final String loginStatus;
+
+        loginStatus(String loginStatus) {
+            this.loginStatus = loginStatus;
+        }
+    }
+
+
+}
