b5a054aeb42103385f86a17edbe611fd0cdea832
Choiyu330 <jenny4063@naver.com>
2023-01-06 14:48:31 +0900

    feat: PostController 생성

---
+++ server/src/main/java/com/seb41_main_018/mainproject/post/controller/PostController.java
@@ -0,0 +1,44 @@
+package com.seb41_main_018.mainproject.post.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Validated
+@RequestMapping("/post")
+public class PostController {
+
+    // 게시글 생성
+    @PostMapping
+    public ResponseEntity postPost(
+            // TODO: DTO
+    ) {
+        return null;
+    }
+
+    // 게시글 단건 조회
+    @GetMapping("/{postId}")
+    public ResponseEntity getPost(@PathVariable("postId") Long postId
+    ) {
+        return null;
+    }
+
+    // 게시글 전체 조회
+    @GetMapping
+    public ResponseEntity getPosts(
+    ) {
+        return null;
+    }
+
+    // 게시글 수정
+    @PatchMapping("/{postId}")
+    public ResponseEntity patchPost(@PathVariable("postId") Long postId
+            // TODO: DTO
+    ) {
+        return null;
+    }
+
+
+
+}
