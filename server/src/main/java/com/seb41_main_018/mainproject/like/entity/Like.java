7760e65f7f9c53caa10741f9fb037c5c0d5e4109
ahn <201717504@kyonggi.ac.kr>
2023-01-06 16:57:10 +0900

    refactor: Like entity

--- server/src/main/java/com/seb41_main_018/mainproject/like/entity/Like.java
+++ server/src/main/java/com/seb41_main_018/mainproject/like/entity/Like.java
@@ -1,11 +1,12 @@
-package com.seb41_main_018.mainproject.like;
+package com.seb41_main_018.mainproject.like.entity;
 
+import com.seb41_main_018.mainproject.audit.Auditable;
+import com.seb41_main_018.mainproject.content.entity.Content;
 import com.seb41_main_018.mainproject.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.domain.Auditable;
 
 import javax.persistence.*;
 
@@ -25,8 +26,8 @@ public class Like extends Auditable {
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
-    private Post post;
+    @JoinColumn(name = "content_id")
+    private Content content;
 
     @Column(nullable = false)
     private int like_count;
