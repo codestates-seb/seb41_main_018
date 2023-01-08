fe934e29bdd9b2a7a663151e88055d68b102d6e4
Choiyu330 <jenny4063@naver.com>
2023-01-09 01:10:02 +0900

    refactor: Content / Tag Entity 수정

--- server/src/main/java/com/seb41_main_018/mainproject/content/entity/Content.java
+++ server/src/main/java/com/seb41_main_018/mainproject/content/entity/Content.java
@@ -2,55 +2,91 @@ package com.seb41_main_018.mainproject.content.entity;
 
 import com.seb41_main_018.mainproject.audit.Auditable;
 import com.seb41_main_018.mainproject.comment.entity.Comment;
-import com.seb41_main_018.mainproject.like.Like;
+import com.seb41_main_018.mainproject.like.entity.Like;
 import com.seb41_main_018.mainproject.route.entity.Route;
 import com.seb41_main_018.mainproject.tag.entity.Tag;
 import com.seb41_main_018.mainproject.user.entity.User;
 import jdk.jfr.Category;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
-@Table
-@EntityListeners(AuditingEntityListener.class)
 @Entity
+@NoArgsConstructor
 public class Content extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contentId;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String title;
 
     @Column(nullable = false)
     private String body;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private Long viewCount;
 
 
-    // 연관 관계
+    // 연관 관계 //
+    @ToString.Exclude
+    @OrderBy("likeId")
+    @OneToMany(mappedBy = "content", cascade = CascadeType.REMOVE)
+    private List<Like> likes = new ArrayList<>();
+
+    @ToString.Exclude
+    @OrderBy("commentId")
+    @OneToMany(mappedBy = "content", cascade = CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<>();
+
+    @ToString.Exclude
+    @OrderBy("routeId")
+    @OneToMany(mappedBy = "content", cascade = CascadeType.REMOVE)
+    private List<Route> routes = new ArrayList<>();
+
+    @ToString.Exclude
+    @OrderBy("tagId")
+    @OneToMany(mappedBy = "content", cascade = CascadeType.REMOVE)
+    private List<Tag> tags = new ArrayList<>();
+
     @ManyToOne(optional = true, fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "USER_ID")
     private User user;
 
     @ManyToOne
+    @JoinColumn(name = "CATEGORY_ID")
     private Category category;
-    @OneToMany
-    private Like like;
 
-    @OneToMany
-    private Tag tag;
+    // 생성자 //
+    public Content(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
 
-    @OneToMany
-    private Comment comment;
+    // 연관관계 메소드 //
+    public void addLike(Like like) {
+        likes.add(like);
+    }
 
-    @OneToMany
-    private Route route;
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void addRoute(Route route) {
+        routes.add(route);
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
+        tag.addContent(this);
+    }
 
 }
