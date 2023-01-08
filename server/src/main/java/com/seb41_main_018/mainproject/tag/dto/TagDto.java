4605ea419c2e84e3db7fd46fe3621b24c37b127c
Choiyu330 <jenny4063@naver.com>
2023-01-09 02:22:31 +0900

    feat: Content / Tag DTO 구현

---
+++ server/src/main/java/com/seb41_main_018/mainproject/tag/dto/TagDto.java
@@ -0,0 +1,32 @@
+package com.seb41_main_018.mainproject.tag.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+public class TagDto {
+    @AllArgsConstructor
+    @Getter
+    public static class TagPost {
+        private Long contentId;
+        @NotBlank(message = "태그명을 입력해야합니다.")
+        private String name;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class TagPatch {
+        private Long tagId;
+        private Long contentId;
+        @NotBlank(message = "태그명을 입력해야합니다.")
+        private String name;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class TagResponse {
+        private Long tagId;
+        private Long contentId;
+        private String name;
+    }
+}
