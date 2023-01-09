0ef93adb113e0528122c2fa181bf3ebf06807714
jinwon kang <dkssud8253@gmail.com>
2023-01-09 17:43:28 +0900

    chore: 컴포넌트, 페이지 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img
-                    src={logo}
-                    className="App-logo"
-                    alt="logo"
-                />
-                <p>
-                    Edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router basename="/">
+            <ScrollToTop />
+            <Header />
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            <Route path="/detail" element={<Detail />}></Route>
+            <Route path="/post" element={<Post />}></Route>
+            <Route path="/result" element={<Result />}></Route>
+        </Router>
     );
-}
+};
 
 export default App;
