4d6a9d39b0c14593a988676945ed881b1fbd26c4
ahn <201717504@kyonggi.ac.kr>
2023-01-09 11:31:23 +0900

    feat: UserPostDto

---
+++ server/src/main/java/com/seb41_main_018/mainproject/user/dto/UserPostDto.java
@@ -0,0 +1,26 @@
+package com.seb41_main_018.mainproject.user.dto;
+
+import com.seb41_main_018.mainproject.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UserPostDto {
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String ninkname;
+    @NotBlank
+    private Boolean email_subscribe;
+}
