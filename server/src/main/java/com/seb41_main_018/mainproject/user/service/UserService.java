cf145deb0eca3dc80a1294411c3ab2eb66f3074e
ahn <201717504@kyonggi.ac.kr>
2023-01-09 11:31:47 +0900

    feat: UserService

---
+++ server/src/main/java/com/seb41_main_018/mainproject/user/service/UserService.java
@@ -0,0 +1,71 @@
+package com.seb41_main_018.mainproject.user.service;
+
+import com.seb41_main_018.mainproject.exception.BusinessLogicException;
+import com.seb41_main_018.mainproject.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import com.seb41_main_018.mainproject.user.entity.User;
+import com.seb41_main_018.mainproject.exception.ExceptionCode;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    //private final CustomBeanUtils<Member> beanUtils;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        // 이미 등록된 이메일인지 확인
+        verifyExistsEmail(user.getEmail());
+
+        return userRepository.save(user);
+    }
+    //유저 수정
+    public User updateUser(User user) {
+        User findUser = findVerifiedUser(user.getUserId()); //ID로 멤버 존재 확인하고 User 정보 반환
+
+        Optional.ofNullable(user.getNinkname())
+                .ifPresent(nickname -> findUser.setNinkname(nickname));
+        //Optional.ofNullable(user.getPassword())
+                //.ifPresent(password -> findUser.setPassword(bCryptPasswordEncoder.encode(password)));
+
+        return userRepository.save(findUser);
+    }
+
+    public User findUser(long userId) {
+        return findVerifiedUser(userId);
+    }
+
+    public Page<User> findMembers(int page, int size) {
+        return userRepository.findAll(PageRequest.of(page, size,
+                Sort.by("userId").descending()));
+    }
+
+    //유저 삭제
+    public void deleteUser(long userId) {
+        User findUser = findVerifiedUser(userId);
+
+        userRepository.delete(findUser);
+    }
+
+    public User findVerifiedUser(long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        User findUser =
+                optionalUser.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        return findUser;
+    }
+
+    private void verifyExistsEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent())
+            throw new BusinessLogicException(ExceptionCode.USER_EXISTS);
+    }
+}
