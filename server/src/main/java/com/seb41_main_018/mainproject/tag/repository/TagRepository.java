6cb3879e0d15c8bc59ad880a8d42748a6a53a0ad
Choiyu330 <jenny4063@naver.com>
2023-01-09 14:38:43 +0900

    feat: TagRepository 구현

---
+++ server/src/main/java/com/seb41_main_018/mainproject/tag/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.seb41_main_018.mainproject.tag.repository;
+
+import com.seb41_main_018.mainproject.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
