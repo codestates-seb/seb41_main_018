890f8ed6c90153dc9984b1836e048a20b7803c29
ahn <201717504@kyonggi.ac.kr>
2023-01-09 11:30:52 +0900

    feat: UserAllResponseDto

---
+++ server/src/main/java/com/seb41_main_018/mainproject/user/dto/UserAllResponseDto.java
@@ -0,0 +1,27 @@
+package com.seb41_main_018.mainproject.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class UserAllResponseDto {
+    private Long userId;
+    private String email;
+    private String ninkname;
+    private String password;
+    private Boolean email_subscribe;
+
+    //private List<ContentResponseDto> contents;
+
+    //private List<CommentResponseDto> comments;
+
+    //private List<LikeResponseDto> likes;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+}
