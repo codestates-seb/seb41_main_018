e115a39b33ab4a8f3679f1ee17cf8500faf29eb5
Choiyu330 <jenny4063@naver.com>
2023-01-09 15:08:03 +0900

    feat: ContentRepository 수정

--- server/src/main/java/com/seb41_main_018/mainproject/content/repository/ContentRepository.java
+++ server/src/main/java/com/seb41_main_018/mainproject/content/repository/ContentRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
+    Optional<Content> findByContentId(Long contentId);
 }
