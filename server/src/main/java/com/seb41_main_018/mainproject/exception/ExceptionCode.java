e8b91df7b3607c633c60c47b27b67236299cfcdd
kangharyeom <kangharyeom@gmail.com>
2023-01-09 15:03:56 +0900

    BE: Feat Comment, Category Mapper

--- server/src/main/java/com/seb41_main_018/mainproject/exception/ExceptionCode.java
+++ server/src/main/java/com/seb41_main_018/mainproject/exception/ExceptionCode.java
@@ -6,17 +6,20 @@ public enum ExceptionCode {
     ANSWER_NOT_FOUND(404, "Answer not found"),
     LIKE_NOT_FOUND(404, "LIKE not found"),
     COMMENT_NOT_FOUND(404, "Comment not found"),
+    CATEGORY_NOT_FOUND(404, "CATEGORY not found"),
     USER_NOT_FOUND(404, "USER not found"),
     POST_NOT_FOUND(404, "POST not found"),
     ANSWER_EXISTS(409, "Answer exists"),
     USER_EXISTS(409, "USER exists"),
     POST_EXISTS(409, "POST exists"),
+    COMMENT_EXISTS(409, "POST exists" ),  // TO 추가된 부분
+    CATEGORY_EXISTS(409, "CATEGORY exists" ),  // TO 추가된 부분
     CANNOT_CHANGE_ANSWER(403, "Answer can not change"),
     CANNOT_CHANGE_POST(403, "POST can not change"),
     CANNOT_CHANGE_COMMENT(403,"Comment can not change"),
     CANNOT_CHANGE_USER(403, "USER can not change"),
     NOT_IMPLEMENTATION(501, "Not Implementation"),
-    INVALID_USER_STATUS(400, "Invalid USER status");  // TO 추가된 부분
+    INVALID_USER_STATUS(400, "Invalid USER status");
 
     @Getter
     private int status;
