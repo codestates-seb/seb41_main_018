7f074f9076eab04e75576577aea471299c341b19
Choiyu330 <jenny4063@naver.com>
2023-01-10 11:21:03 +0900

    feat: RouteRepository 구현

---
+++ server/src/main/java/com/seb41_main_018/mainproject/route/repository/RouteRepository.java
@@ -0,0 +1,7 @@
+package com.seb41_main_018.mainproject.route.repository;
+
+import com.seb41_main_018.mainproject.route.entity.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
