898736d378a6fa2df2278dfa4fd466c22e759732
Choiyu330 <jenny4063@naver.com>
2023-01-10 11:19:58 +0900

    refactor: Route Entity 수정

--- server/src/main/java/com/seb41_main_018/mainproject/route/entity/Route.java
+++ server/src/main/java/com/seb41_main_018/mainproject/route/entity/Route.java
@@ -2,6 +2,7 @@ package com.seb41_main_018.mainproject.route.entity;
 
 import com.seb41_main_018.mainproject.audit.Auditable;
 import com.seb41_main_018.mainproject.content.entity.Content;
+import com.seb41_main_018.mainproject.routeplace.entity.RoutePlace;
 import com.seb41_main_018.mainproject.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -21,10 +24,19 @@ public class Route extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long routeId;
 
+    @Column(nullable = false)
+    private String name;
+
+//    @OrderBy("routeplaceId")
+//    @OneToMany(mappedBy = "route", cascade = CascadeType.REMOVE)
+//    private List<RoutePlace> routePlaces = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
+    @JoinColumn(name = "CONTENT_ID")
     private Content content;
 
-    @Column(nullable = false)
-    private String body;
+    // 생성자 //
+    public Route(String name) {
+        this.name = name;
+    }
 }
