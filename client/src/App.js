798b606efe67f4201a240b37980dca44274d7957
RiOAOn <rian0422@naver.com>
2023-01-10 11:00:20 +0900

    fix: browser error 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "../src/util/ScrollToTop";
+import Header from "./components/Header";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import SignUp from "../src/pages/SignUp";
+import Mypage from "../src/pages/Mypage";
+import Detail from "../src/pages/Detail";
+import Post from "../src/pages/Post";
+import Result from "../src/pages/Result";
+
 const App = () => {
     return (
-        <Router basename="/">
+        <BrowserRouter basename="/">
             <ScrollToTop />
             <Header />
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/mypage" element={<Mypage />}></Route>
-            <Route path="/detail" element={<Detail />}></Route>
-            <Route path="/post" element={<Post />}></Route>
-            <Route path="/result" element={<Result />}></Route>
-        </Router>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/signup" element={<SignUp />}></Route>
+                <Route path="/mypage" element={<Mypage />}></Route>
+                <Route path="/detail" element={<Detail />}></Route>
+                <Route path="/post" element={<Post />}></Route>
+                <Route path="/result" element={<Result />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
