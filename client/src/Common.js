bf108b95c93600772e80eb8437128bce1afb587c
진원 <89727516+z1nun@users.noreply.github.com>
2023-01-12 14:53:46 +0900

    CommonJS

---
+++ client/src/Common.js
@@ -0,0 +1,21 @@
+export const PALETTE = {
+    //text
+    text_default: "#000000",
+    text_placeholder: "#b4b4b4",
+
+    /* identity */
+    default_color: "#1e9382",
+    default_hover: "#65bcbe",
+    default_active: "#097C7C",
+    second_color: "#09697c",
+
+    /* layout */
+    border_color: "1px solid rgba(0, 0, 0, 0.3)",
+    border_radius: "5px",
+    border_round: "50px",
+
+    /* Etc */
+    gray: "#787878",
+    ligth_gray: "#bebebe",
+    white: "#fff",
+};
