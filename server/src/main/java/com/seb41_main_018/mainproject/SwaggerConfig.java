1da1fac9af86a0e42e994752cb7f9e3e53481c48
kangharyeom <kangharyeom@gmail.com>
2023-01-16 17:45:28 +0900

    BE: Refactor Swagger none use

--- server/src/main/java/com/seb41_main_018/mainproject/SwaggerConfig.java
+++ server/src/main/java/com/seb41_main_018/mainproject/SwaggerConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.seb41_main_018.mainproject;
 
 import org.springframework.context.annotation.Bean;
@@ -20,3 +21,4 @@ public class SwaggerConfig {
                 .build();
     }
 }
+*/
