b6732ad24cd84eaba8336ae56f5c4f164a736499
진원 <89727516+z1nun@users.noreply.github.com>
2023-01-17 15:40:56 +0900

    feat: Detail form 구현중

---
+++ client/src/components/Detailform.js
@@ -0,0 +1,82 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { PALETTE } from "../Common";
+import { css } from "@emotion/react";
+import DetailformItems from "./DetailformItems";
+import Button from "../components/Button";
+import FiShare from "react-icons/fi";
+
+const Detailform = (props) => {
+    return (
+        <div
+            css={css`
+                position: sticky;
+                top: 50px;
+                right: 10px;
+                height: 100%;
+            `}
+        >
+            <div
+                css={css`
+                    width: 30vw;
+                    border-radius: ${PALETTE.border_radius};
+                    box-shadow: 2px 2px 10px 2px rgb(0, 0, 0, 0.2);
+                    margin: 10px 40px;
+                `}
+            >
+                <div>
+                    <DetailformItems text="강남역" />
+                    <DetailformItems text="역삼역" />
+                    <DetailformItems text="삼성역" />
+                </div>
+
+                {/* 공통 정보 */}
+                <div css={ContentsBody}>
+                    <div css={ComContent}>
+                        <span>카테고리</span>
+                        <span>혼자 여행</span>
+                    </div>
+                    <div css={ComContent}>
+                        <span>여행일</span>
+                        <span>2023.02.08</span>
+                    </div>
+                    <div css={ComContent}>
+                        <span>총 여행 경비</span>
+                        <span>700,000원</span>
+                    </div>
+                    <div css={TagBox}>
+                        <div>맛집</div>
+                        <div>혼밥</div>
+                        <div>사람 많음ㅜㅜ</div>
+                    </div>
+                </div>
+            </div>
+            <Button width="100px" text="가치갈래?" />
+            <Button width="80px" text="<FiShare/>" />
+        </div>
+    );
+};
+
+const ContentsBody = css`
+    padding-top: 20px;
+`;
+
+const ComContent = css`
+    display: flex;
+    justify-content: space-between;
+    padding: 3px 20px;
+`;
+
+const TagBox = css`
+    display: flex;
+    padding: 10px 15px;
+    div {
+        background-color: #e1ecf4;
+        border-radius: 3px;
+        padding: 5px;
+        margin: 5px;
+        color: #39739d;
+        font-size: 12px;
+    }
+`;
+export default Detailform;
---
+++ client/src/components/DetailformItems.js
@@ -0,0 +1,89 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { useState } from "react";
+import { PALETTE } from "../Common";
+import { css } from "@emotion/react";
+
+const DetailformItems = (props) => {
+    const [isClick, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!isClick);
+    };
+    return (
+        <>
+            <div onClick={handleClick} css={wrap}>
+                {props.text}
+                {isClick ? (
+                    <div css={Clicked}>
+                        <ul>
+                            <li>
+                                <span>경비</span>
+                                <div>50000원</div>
+                            </li>
+                            <li>
+                                <span>이동 수단</span>
+                                <div>자동차</div>
+                            </li>
+                            <li>
+                                <span>상세 설명</span>
+                                <div> 아이들도 입장 가능합니다! </div>
+                            </li>
+                        </ul>
+                    </div>
+                ) : (
+                    false
+                )}
+            </div>
+
+            <div></div>
+        </>
+    );
+};
+
+const wrap = css`
+    font-size: 1.4rem;
+    font-weight: 600;
+    text-align: center;
+    border: 2px solid ${PALETTE.default_color};
+    border-radius: ${PALETTE.border_radius};
+    width: 27vw;
+    margin: 30px auto;
+    padding: 5px;
+`;
+
+const Clicked = css`
+    width: 25vw;
+    margin: 5px auto;
+    text-align: start;
+    border-radius: ${PALETTE.border_radius};
+    font-size: 1.2rem;
+    font-weight: 400;
+    animation: identifier 1s ease-in-out;
+
+    @keyframes identifier {
+        0% {
+            max-height: 0px;
+            opacity: 0;
+        }
+        100% {
+            max-height: 300px;
+            opacity: 1;
+        }
+    }
+    li {
+        margin: 30px 0;
+    }
+
+    span {
+        color: ${PALETTE.gray};
+        font-weight: 600;
+        font-size: 0.9rem;
+        margin: 10px;
+    }
+    div {
+        font-size: 1rem;
+        /* border-bottom: 1px solid red; */
+        margin: 10px;
+    }
+`;
+export default DetailformItems;
