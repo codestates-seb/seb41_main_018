2e8146263c044c3fa4c4b920d13e6c4c056c7c4e
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-01-18 17:41:02 +0900

    Revert "BE: 1차 배포" (#129)
    This reverts commit 282475c4f0cf03df9e7a1b374a788e688bb37b75.

--- server/src/main/java/com/seb41_main_018/mainproject/MainprojectApplication.java
+++ server/src/main/java/com/seb41_main_018/mainproject/MainprojectApplication.java
@@ -2,19 +2,12 @@ package com.seb41_main_018.mainproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
-public class MainprojectApplication extends SpringBootServletInitializer {
+public class MainprojectApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(MainprojectApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(MainprojectApplication.class);
-	}
 }
--- server/src/main/java/com/seb41_main_018/mainproject/auth/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.seb41_main_018.mainproject.auth.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String email;
-    private String password;
-}
