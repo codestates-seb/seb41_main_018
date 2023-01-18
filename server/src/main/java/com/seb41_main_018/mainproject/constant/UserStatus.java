55990af2774f6cfb73d4f10809bca52f66b0e8c9
Kangharyeom <108250233+kangharyeom@users.noreply.github.com>
2023-01-18 17:39:19 +0900

    Revert "BE: 1차 배포 (feat/be -> dev)"

--- server/src/main/java/com/seb41_main_018/mainproject/constant/UserStatus.java
+++
@@ -1,7 +0,0 @@
-package com.seb41_main_018.mainproject.constant;
-
-public enum UserStatus {
-    ACTIVITY,
-    INACTIVITY,
-    QUIT
-}
--- server/src/main/java/com/seb41_main_018/mainproject/tag/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seb41_main_018.mainproject.tag.repository;
-
-import com.seb41_main_018.mainproject.tag.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
