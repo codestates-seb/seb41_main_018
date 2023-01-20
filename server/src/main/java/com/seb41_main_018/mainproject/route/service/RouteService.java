a04e4d16e890c705d6d3bceb7c64953651134a39
ahn <201717504@kyonggi.ac.kr>
2023-01-21 03:05:06 +0900

    refactor : Route

--- server/src/main/java/com/seb41_main_018/mainproject/route/service/RouteService.java
+++ server/src/main/java/com/seb41_main_018/mainproject/route/service/RouteService.java
@@ -1,88 +1,71 @@
 package com.seb41_main_018.mainproject.route.service;
 
 import com.seb41_main_018.mainproject.content.entity.Content;
+import com.seb41_main_018.mainproject.content.repository.ContentRepository;
 import com.seb41_main_018.mainproject.content.service.ContentService;
 import com.seb41_main_018.mainproject.exception.BusinessLogicException;
 import com.seb41_main_018.mainproject.exception.ExceptionCode;
-import com.seb41_main_018.mainproject.response.SingleResponseDto;
-import com.seb41_main_018.mainproject.route.dto.RouteDto;
 import com.seb41_main_018.mainproject.route.entity.Route;
-import com.seb41_main_018.mainproject.route.mapper.RouteMapper;
 import com.seb41_main_018.mainproject.route.repository.RouteRepository;
-import com.seb41_main_018.mainproject.routeplace.repository.RoutePlaceRepository;
+import com.seb41_main_018.mainproject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class RouteService {
-    private final RouteRepository routeRepository;
+    private final UserRepository userRepository;
     private final ContentService contentService;
-    private final RouteMapper routeMapper;
-    private final RoutePlaceRepository routePlaceRepository;
+    private final RouteRepository routeRepository;
+    private final ContentRepository contentRepository;
 
-
-    // 경로 생성 //
+    // 루트 장소 생성 //
     public Route createRoute(Route route, Long contentId) {
         Content content = contentService.findVerifiedContent(contentId);
         route.setContent(content);
+//        route.setTotalPrice(route.getTotalPrice()+routePlace.getPrice());
+//        routeRepository.save(route);
 
         return routeRepository.save(route);
     }
 
-    // 경로 수정 //
-    public Route updateRoute(Long routeId, Route route) {
-        Route findRoute = findVerifiedRoute(routeId);
+//    // 루트 장소 수정 //
+//    public Route updateRoute(Long routeId, Route route) {
+//        Route findRoute = findVerifiedRoute(routeId);
+//
+//        Optional.ofNullable(routePlace.getBody())
+//                .ifPresent(findPlace::setBody);
+//
+//        return routePlaceRepository.save(findPlace);
+//    }
 
-//        Optional.ofNullable(route.getName())
-//                .ifPresent(findRoute::setName);
-        Optional.ofNullable(route.getDate())
-                .ifPresent(findRoute::setDate);
-
-        return routeRepository.save(findRoute);
-    }
-
-    // 경로 단건 조회 //
-    public Route findRoute(Long routeId) {
-        return findVerifiedRoute(routeId);
-    }
-
-    // 경로 전체 조회 //
+    // 루트 장소 전체 조회 //
     public Page<Route> findRoutes(int page, int size) {
         return routeRepository.findAll(PageRequest.of(page, size,
-                Sort.by("routeId").descending()));
+                Sort.by("placeId").descending()));
     }
 
-    // 경로 삭제 //
+    // 루트 장소 조회 //
+    public Route findRoute(Long routeId){
+        return findVerifiedRoute(routeId);
+    }
+    // 루트 장소 삭제 //
     public void deleteRoute(Long routeId) {
         Route findRoute = findVerifiedRoute(routeId);
         routeRepository.delete(findRoute);
     }
 
-    // 경로 검증 로직 //
-    public Route findVerifiedRoute(Long routeId) {
-        Optional<Route> optionalRoute = routeRepository.findById(routeId);
+    // 루트 장소 검증 로직 //
+    public Route findVerifiedRoute(Long routeId){
+        Optional<Route> optionalRoute = routeRepository.findByRouteId(routeId);
         Route findRoute = optionalRoute.orElseThrow(() ->
-                        new BusinessLogicException(ExceptionCode.ROUTE_NOT_FOUND));
+                new BusinessLogicException(ExceptionCode.ROUTE_PLACE_NOT_FOUND));
 
         return findRoute;
-    }
-    @Transactional(readOnly = true)
-    public ResponseEntity detail(Route route) {
-
-        RouteDto.RouteResponse routeResponse = routeMapper.routeToRouteResponse(route,routePlaceRepository);
-
-        return new ResponseEntity<>(
-                new SingleResponseDto<>(routeResponse), HttpStatus.OK
-        );
     }
 }
