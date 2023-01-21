3fa079b7bc3d86e2e1dc7996cb29e5b7078be084
ahn <201717504@kyonggi.ac.kr>
2023-01-22 03:28:49 +0900

    refactor : Comment

--- server/src/main/java/com/seb41_main_018/mainproject/comment/repository/CommentRepository.java
+++ server/src/main/java/com/seb41_main_018/mainproject/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.seb41_main_018.mainproject.comment.repository;
 
 import com.seb41_main_018.mainproject.comment.entity.Comment;
+import com.seb41_main_018.mainproject.content.entity.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +11,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query(value = "select * from comment where content_id = :contentId", nativeQuery = true)
     List<Comment> findAllByContentId(long contentId);
+
+    @Query(value = "select * from comment where user_id = :userId", nativeQuery = true)
+    List<Comment> findAllByUserId(long userId);
 }
