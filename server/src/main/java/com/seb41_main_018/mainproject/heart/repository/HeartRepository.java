a74011d29d997b854b9e063aa8f672f183ddc8d2
ahn <201717504@kyonggi.ac.kr>
2023-01-22 03:28:29 +0900

    refactor : Heart

--- server/src/main/java/com/seb41_main_018/mainproject/heart/repository/HeartRepository.java
+++ server/src/main/java/com/seb41_main_018/mainproject/heart/repository/HeartRepository.java
@@ -1,9 +1,11 @@
 package com.seb41_main_018.mainproject.heart.repository;
 
+import com.seb41_main_018.mainproject.constant.HeartType;
 import com.seb41_main_018.mainproject.content.entity.Content;
 import com.seb41_main_018.mainproject.heart.entity.Heart;
 import com.seb41_main_018.mainproject.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,7 @@ public interface HeartRepository extends JpaRepository<Heart, Long> {
 //    List<Heart> findAllByUserAndContent(User user, Content content);
 //    List<Heart> findAllByContent(Content content);
 Optional<Heart> findByUserAndContent(User user, Content content);
+
+    @Query(value = "select * from heart where user_id = :userId", nativeQuery = true)
+    List<Heart> findAllByUserId(long userId);
 }
