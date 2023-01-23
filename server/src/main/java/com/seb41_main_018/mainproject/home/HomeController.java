561c9bfe354ce975b6c74efd94341b46433d1fbb
Choiyu330 <jenny4063@naver.com>
2023-01-23 22:15:17 +0900

    refactor: yml 수정 / HomeController 생성

---
+++ server/src/main/java/com/seb41_main_018/mainproject/home/HomeController.java
@@ -0,0 +1,18 @@
+package com.seb41_main_018.mainproject.home;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity helloWorld() {
+        return ResponseEntity.ok().body(
+                Map.of("hello", "world")
+        );
+    }
+
+}
