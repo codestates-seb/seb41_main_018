3715a71b97b4024f92a4d1d02ee9e002792b0144
진원 <89727516+z1nun@users.noreply.github.com>
2023-01-24 21:13:02 +0900

    Co-authored-by: RiOAOn <RiOAOn@users.noreply.github.com>

--- client/src/util/axiosDetail.js
+++ client/src/util/axiosDetail.js
@@ -1,16 +1,12 @@
-import axios from "axios";
-
-// http://ec2-54-180-87-83.ap-northeast-2.compute.amazonaws.com:8080
-
-// 컨텐트 전체 조회 & 컨텐트 조회
 export const getContent = async (contentId = 0) => {
+    const [contentDetail, setContentDetail] = useRecoilState(ContentDetail);
     await axios
         .get(`/constents${contentId !== 0 ? `/${contentId}` : ""}`)
         .then((res) => {
             if (!res.ok) {
                 throw Error("error");
             }
-            return res;
+            return res.json();
         })
         .catch((err) => {
             console.error(err.message);
