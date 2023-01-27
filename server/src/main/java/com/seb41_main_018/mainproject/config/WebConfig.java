373d97ceb5da5b4eb2b2ad2b4a5e7f2724573638
Choiyu330 <jenny4063@naver.com>
2023-01-28 00:00:32 +0900

    feat: CORS 오류 - WebConfig Class

---
+++ server/src/main/java/com/seb41_main_018/mainproject/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.seb41_main_018.mainproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// CORS 에러 방지 //
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOriginPatterns("")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken");
+                //.allowCredentials(true);
+            }
+        };
+    }
+}
