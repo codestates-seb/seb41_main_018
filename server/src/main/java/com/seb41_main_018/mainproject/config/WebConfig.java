45bd3c464de8c1555f1510e4b960052bc5260398
ahn <201717504@kyonggi.ac.kr>
2023-01-28 00:33:59 +0900

    refactor : WebConfig

--- server/src/main/java/com/seb41_main_018/mainproject/config/WebConfig.java
+++ server/src/main/java/com/seb41_main_018/mainproject/config/WebConfig.java
@@ -1,25 +1,21 @@
-package com.seb41_main_018.mainproject.config;
+package sebpre018.com.stackOverflowClone.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// CORS 에러 방지 //
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
-                        .allowedOriginPatterns("")
-                        .allowedHeaders("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-                        .exposedHeaders("Authorization", "RefreshToken");
-                //.allowCredentials(true);
-            }
-        };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry
+                .addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Refresh")
+                .allowCredentials(true).maxAge(3600);
     }
 }
