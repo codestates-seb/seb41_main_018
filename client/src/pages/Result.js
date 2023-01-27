afa54866755782bbf7e71f4f2b8360cc2a31f9d7
RiOAOn <rian0422@naver.com>
2023-01-27 15:26:13 +0900

    feat: 카테고리 선택 검색 기능

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -3,14 +3,25 @@ import React from "react";
 import { css } from "@emotion/react";
 import Categorybar from "../components/Categorybar";
 import HomeItems from "../components/Home_components/HomeItems";
+import { useRecoilState } from "recoil";
+import { CategorySearchResultState } from "../../src/state/atom";
 
 const Result = () => {
+    const [categorySearch, setCategorySearch] = useRecoilState(CategorySearchResultState);
     return (
         <div>
             <Categorybar />
             <div>
                 <div css={resultText}>검색결과</div>
-                <div css={postStyle}>
+                {console.log(categorySearch)}
+                {categorySearch.map((content) => (
+                    <div css={postStyle}>
+                        {/* 백엔드에서 요소 추가 완료 후 주석 제거
+						  			 현재 개수만 확인 가능 */}
+                        <HomeItems /* content={content} */ />
+                    </div>
+                ))}
+                {/* <div css={postStyle}>
                     <HomeItems />
                     <HomeItems />
                     <HomeItems />
@@ -27,7 +38,7 @@ const Result = () => {
                     <HomeItems />
                     <HomeItems />
                     <HomeItems />
-                </div>
+                </div> */}
             </div>
         </div>
     );
