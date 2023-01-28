baf213f7cb3a00c243bf622ae69f31d2de374732
RiOAOn <rian0422@naver.com>
2023-01-29 00:15:40 +0900

    feat: 제목 Link 연결

--- client/src/components/Mypage_components/MyReview.js
+++ client/src/components/Mypage_components/MyReview.js
@@ -5,6 +5,7 @@ import { PALETTE } from "../../Common";
 import dayjs from "dayjs";
 import { userInfoState } from "../../state/atom";
 import { useRecoilState } from "recoil";
+import { Link } from "react-router-dom";
 
 const MyReview = () => {
     const [userInfo, setUserInfo] = useRecoilState(userInfoState);
@@ -15,7 +16,10 @@ const MyReview = () => {
                 <div css={MyReview_Item} key={review.reviewId}>
                     <div css={PostImg}>사진</div>
                     <div css={MyReview_Content}>
-                        <h3 css={PostTitle}>{review.title}</h3>
+                        {console.log(review.contentId)}
+                        <Link to={`/detail/${review.contentId}`}>
+                            <h3 css={PostTitle}>{review.title}</h3>
+                        </Link>
                         <div>{review.body}</div>
                         <div css={Right_Content}>{dayjs(review.createdAt).format("YY.MM.DD")}</div>
                     </div>
