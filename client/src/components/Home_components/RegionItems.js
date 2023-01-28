f8273954f0914e4087dac56fdbb1aad8ef2b5017
진원 <89727516+z1nun@users.noreply.github.com>
2023-01-29 05:05:28 +0900

    Style : 지역아이템 사진

--- client/src/components/Home_components/RegionItems.js
+++ client/src/components/Home_components/RegionItems.js
@@ -1,12 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "../Button";
 import { css } from "@emotion/react";
 import { PALETTE } from "../../Common";
-import 경복궁 from "../../assets/sampleImg/경복궁.png";
-import SignButton from "../SignButton";
 
-const Regionitems = () => {
+import { useRecoilState } from "recoil";
+import { ContentsList } from "../../state/atom";
+
+const Regionitems = (props) => {
+    const [contentsList, setcontentsList] = useRecoilState(ContentsList);
+    const handleClick = () => {};
+
     return (
         <div
             css={css`
@@ -14,16 +18,17 @@ const Regionitems = () => {
             `}
         >
             <div>
-                <img src={경복궁} css={imgStyle} />
+                <img src={props.img} css={imgStyle} />
             </div>
             <div css={textWrap}>
-                <div css={textStyle}>서울</div>
+                <div css={textStyle}>{props.text}</div>
                 <Button
+                    bgColor="white"
                     width="100px"
                     height="40px"
                     text="더보기"
                     color="black"
-                    boxShadow="1px 1px 5px rgb(0,0,0,0.2)"
+                    onClick={handleClick}
                 />
             </div>
         </div>
@@ -31,54 +36,27 @@ const Regionitems = () => {
 };
 
 const imgStyle = css`
-    width: 275px;
-    height: 380px;
-    margin: 10px 0;
+    width: 100%;
+    min-height: 340px;
+    height: 42vh;
     border-radius: ${PALETTE.border_radius};
-    @media (max-width: 1200px) {
-        width: 250px;
-        height: 320px;
-    }
-    @media (max-width: 1000px) {
-        width: 250px;
-        height: 320px;
-    }
-    @media (max-width: 768px) {
-        width: 290px;
-        height: 320px;
-    }
-    @media (max-width: 576px) {
-        width: 460px;
-        height: 320px;
-    }
+    object-fit: cover;
 `;
 
 const textWrap = css`
     position: relative;
-    bottom: 180px;
+    bottom: 9rem;
     left: 20px;
-    @media (max-width: 1200px) {
-        bottom: 150px;
-        left: 20px;
-    }
 `;
 
 const textStyle = css`
     position: relative;
-    font-size: 4rem;
+    font-size: 2.7rem;
     font-weight: 600;
-    color: rgb(255, 255, 255, 0.9);
+    color: rgb(255, 255, 255, 0.8);
     text-shadow: ${PALETTE.text_shadow};
-    bottom: 190px;
+    bottom: 11rem;
     left: 0;
-    @media (max-width: 1200px) {
-        font-size: 3rem;
-        bottom: 160px;
-        left: 0;
-    }
-    @media (max-width: 1000px) {
-        font-size: 2.5rem;
-    }
 `;
 
 export default Regionitems;
