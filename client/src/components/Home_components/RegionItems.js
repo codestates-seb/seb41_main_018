09c6bb6585a3affce46a999c283e65a57625b132
진원 <89727516+z1nun@users.noreply.github.com>
2023-01-29 13:31:59 +0900

    style : 지역 아이템 텍스트 그림자 효과 변경

--- client/src/components/Home_components/RegionItems.js
+++ client/src/components/Home_components/RegionItems.js
@@ -53,8 +53,10 @@ const textStyle = css`
     position: relative;
     font-size: 2.7rem;
     font-weight: 600;
-    color: rgb(255, 255, 255, 0.8);
-    text-shadow: ${PALETTE.text_shadow};
+    color: rgb(255, 255, 255, 0.9);
+    text-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
+        rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+        rgba(0, 0, 0, 0.09) 0px -3px 5px;
     bottom: 11rem;
     left: 0;
 `;
