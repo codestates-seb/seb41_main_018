b552c16500b903aff577e08f477609b335d11a95
shinpaka <ksb71202@gmail.com>
2023-01-31 05:25:23 +0900

    content patch addroute bug fix 및 sampleimage파일 추가

---
+++ client/src/sampleImage.js
@@ -0,0 +1,43 @@
+export const GachiGalleImgSrc = {
+    //logo
+    logo_img: "https://i.postimg.cc/zXsF686H/Gachi-Gallae-Logo.png",
+
+    //button
+    left_button_img: "https://i.postimg.cc/XYdFGPLR/left.png",
+    right_button_img: "https://i.postimg.cc/1tCN11Rq/right.png",
+
+    //region
+    seoul_img: "https://i.postimg.cc/CMnjpdDM/image.png",
+    busan_img: "https://i.postimg.cc/dV26P3bZ/image.png",
+    gangneung_img: "https://i.postimg.cc/tJyzSjTG/image.png",
+    damyang_img: "https://i.postimg.cc/wM6h5Qf6/image.png",
+    yeosu_img: "https://i.postimg.cc/t41FzT5V/image.png",
+    jeonju_img: "https://i.postimg.cc/66YRJRbv/image.png",
+    jeju_img: "https://i.postimg.cc/zB9f8zh4/image.png",
+    paju_img: "https://i.postimg.cc/Y03T88t8/image.png",
+    pocheon_img: "https://i.postimg.cc/HWBSZrQS/image.png",
+
+    //sample
+    sample_img: "https://i.postimg.cc/pXjF97XD/sample.jpg",
+    sample_img1_1: "https://i.postimg.cc/VNK4N6zG/sam1-1.png",
+    sample_img1_2: "https://i.postimg.cc/rpZCkP97/sam1-2.jpg",
+    sample_img1_3: "https://i.postimg.cc/Y9mgwq0m/sam1-3.jpg",
+    sample_img2: "https://i.postimg.cc/jSd7ShCT/sam2.jpg",
+    sample_img3_1: "https://i.postimg.cc/8Pk5RzGB/sam3-1.jpg",
+    sample_img3_2: "https://i.postimg.cc/RFQStjbD/sam3-2.jpg",
+
+    //category
+    family_travel: "https://i.postimg.cc/4d5GypYC/image.png",
+    domestic_travel: "https://i.postimg.cc/Wz3B4CRr/image.png",
+    food_travel: "https://i.postimg.cc/d0wS1pr1/image.png",
+    friend_travel: "https://i.postimg.cc/YCkJGvwM/image.png",
+    cafe_travel: "https://i.postimg.cc/ncKWJn95/image.png",
+    couple_travel: "https://i.postimg.cc/xCymdpG0/image.png",
+    overseas_travel: "https://i.postimg.cc/wBFHThtC/image.png",
+    solo_travel: "https://i.postimg.cc/J4Js72RS/image.png",
+
+    //banner
+    banner_1: "https://i.postimg.cc/yNWBxDFW/1.png",
+    banner_2: "https://i.postimg.cc/WzmcCgPY/2.png",
+    banner_3: "https://i.postimg.cc/yd9M3cW6/3.png",
+};
