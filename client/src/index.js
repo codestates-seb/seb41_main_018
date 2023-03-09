c12a534757b1174b6bb9b0b298f5f50e43755ce4
진원 <89727516+z1nun@users.noreply.github.com>
2023-03-09 14:44:00 +0900

    feat : QueryClinetProvider, QueryClinet

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <div>
+    <QueryClientProvider client={queryClient}>
         <RecoilRoot>
             <App />
         </RecoilRoot>
-    </div>
+    </QueryClientProvider>
 );
