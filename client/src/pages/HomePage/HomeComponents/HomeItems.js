86081dc7e5496c13a983b097faab6ab0917d234c
진원 <89727516+z1nun@users.noreply.github.com>
2023-03-30 01:51:25 +0900

    feat : 이미지 resize

--- client/src/pages/HomePage/HomeComponents/HomeItems.js
+++ client/src/pages/HomePage/HomeComponents/HomeItems.js
@@ -78,15 +78,7 @@ const HomeItems = (props) => {
                 )}
             </div>
             <Link to={`/detail/${data && data.contentId}`}>
-                <div
-                // css={css`
-                //     width: 300px;
-                //     height: 200px;
-                //     background-color: red;
-                // `}
-                >
-                    <img src={content.image} css={ImgStyle} />
-                </div>
+                <img src={content.image} css={ImgStyle} />
 
                 <div css={TextWrap}>
                     <div css={TitleStyle}>{data && data.title}</div>
@@ -125,7 +117,6 @@ const Container = css`
 
 const ImgStyle = css`
     width: 100%;
-    height: 200px;
     border-radius: ${PALETTE.border_radius};
     object-fit: cover;
 `;
